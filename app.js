f919480a12fae8ba9e7fa82db3cc7da81d3d143a
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-08-23 21:00:08 +0600

    select section complete

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-// document.getElementById('calculateTotal').addEventListener('click',function(){
-//     const managerCost = document.getElementById('managerSalary');
-//     const managerCostString = managerCost.value;
-//     const managerCostNumber = parseFloat(managerCostString);
-    
-//     const coachCost = document.getElementById('coachSalary');
-//     const coachCostString = coachCost.value;
-//     const coachCostNumber = parseFloat(coachCostString);
-
-//     const finalCost = document.getElementById('totalCost');
-//     finalCost.innerText = managerCostNumber + coachCostNumber;
-// })
+// =============== selection section ==================
 
 const counter = 0;
 const selectBtns = document.getElementsByClassName('select-btn')
@@ -17,7 +6,7 @@ for(const selectBtn of selectBtns){
     selectBtn.addEventListener("click",function(e){
         const targetBtn = e.target;
         const parent = targetBtn.parentNode.parentNode;
-        const targetEventPlayer = parent.querySelector('h5');
+        const targetEventPlayer = parent.querySelector('h2');
         const targetEventPlayerName = targetEventPlayer.innerText
         if(counter >= 5){
             alert("you can't select more than five player");
@@ -25,7 +14,7 @@ for(const selectBtn of selectBtns){
         else{
             createNewLi(targetEventPlayerName)
             targetBtn.disabled =  true;
-            target.classList.add("bg-danger");
+            targetBtn.classList.add("bg-danger");
             counter += 1;
         }
     })
@@ -38,8 +27,10 @@ function createNewLi(text){
 }
 
 
+// ================= total cost section ==============
+
 // ------------- comon function ---------------
-function inputValueById(targetedElementId){
+function inputValueById(targetedElementId) {
     const inputFeildElement = document.getElementById(targetedElementId);
     const inputFeildValue = inputFeildElement.value;
     const inputValue = parseFloat(inputFeildValue);
@@ -48,30 +39,30 @@ function inputValueById(targetedElementId){
 
 const totalPlayer = 5;
 const playerExpensiveCalculate = document.getElementById('calculate-player-expenxive-js')
-playerExpensiveCalculate.addEventListener('click',function(){
+playerExpensiveCalculate.addEventListener('click', function () {
     const perPlayerExpensive = inputValueById('per-player-expensive-js')
-    if(perPlayerExpensive >= 0){
-        const calculate =  totalPlayer*perPlayerExpensive;
+    if (perPlayerExpensive >= 0) {
+        const calculate = totalPlayer * perPlayerExpensive;
         const totalPlayerExpensive = document.getElementById('total-player-expensive-js');
         totalPlayerExpensive.innerText = calculate;
     }
-    else{
+    else {
         alert('please fill input fiels!!!')
     }
 })
 
 
 const calculateTotal = document.getElementById('calculate-total-js')
-calculateTotal.addEventListener('click',function(){
+calculateTotal.addEventListener('click', function () {
     const totalPlayerExepensess = document.getElementById('total-player-expensive-js').innerText;
     const managerCost = inputValueById('manager-expensive-js')
     const coachCost = inputValueById('coach-expensive-js')
     const totalPlayerCost = parseFloat(totalPlayerExepensess)
-    if((managerCost >=0) && (coachCost >=0)){
+    if ((managerCost >= 0) && (coachCost >= 0)) {
         const totalCost = totalPlayerCost + managerCost + coachCost;
         document.getElementById('total-expensive-js').innerText = totalCost;
     }
-    else{
+    else {
         alert('please fill input field')
     }
 })
